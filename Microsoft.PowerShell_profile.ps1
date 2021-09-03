e4584973befbfa0145d8737cb6616ed7e0306cf7
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-09-04 00:42:22 +0800

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+# Set-PoshPrompt -Theme agnosterplus
+#¡@Set-PoshPrompt -Theme blueish
+Set-PoshPrompt -Theme gmay
+# Set-PoshPrompt -Theme jonnychipz
